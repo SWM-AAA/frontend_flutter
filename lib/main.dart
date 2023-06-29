111b37665feca52d91b9c71e190fee74a4e4c036
nakevin96 <nakevin96@hanyang.ac.kr>
2023-06-29 13:26:10 +0900

    [TAS-101] package: kakao Oauth 인증을 위한 패키지 설치 및 AndroidManifest파일 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/common/screens/splash_screen.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 void main() {
+  // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(
+    nativeAppKey: 'ff53a7b39e2479b12df1c386c508e81d',
+  );
+
   runApp(const _App());
 }
 
