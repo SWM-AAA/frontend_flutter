252008730b6416615dca05d103e70447f5c0e6c0
nakevin96 <nakevin96@hanyang.ac.kr>
2023-06-29 15:43:22 +0900

    [TAS-101] feature: kakodeveloper에서 토큰 받아오기
    kakao flutter sdk를 이용하여 accessToken과 refreshToken을 할당받고 이를 보안적으로 안전하게 보관

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:frontend/common/screens/splash_screen.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
-void main() {
+void main() async {
   // 웹 환경에서 카카오 로그인을 정상적으로 완료하려면 runApp() 호출 전 아래 메서드 호출 필요
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: '.env');
+
   // runApp() 호출 전 Flutter SDK 초기화
   KakaoSdk.init(
-    nativeAppKey: 'ff53a7b39e2479b12df1c386c508e81d',
+    nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY']!,
   );
 
   runApp(const _App());
--- lib/user/screens/login_screen.dart
+++ lib/user/screens/login_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:frontend/common/components/animated_app_title.dart';
 import 'package:frontend/common/layouts/default_layout.dart';
 import 'package:frontend/common/screens/root_tab.dart';
 import 'package:frontend/user/screens/components/kakao_login_button.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,12 +16,53 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    void onKakaoLoginButtonClick() {
+    void moveToRootTab() {
       Navigator.of(context).pushAndRemoveUntil(
           MaterialPageRoute(
             builder: (_) => const RootTab(),
           ),
           (route) => false);
+    }
+
+    void onKakaoLoginButtonClick() async {
+      OAuthToken? resp;
+      // INFO: 앱 키 해시 확인 코드
+      // final keyHash = await KakaoSdk.origin;
+      // print('카카오 키해시: $keyHash');
+      if (await isKakaoTalkInstalled()) {
+        try {
+          resp = await UserApi.instance.loginWithKakaoTalk();
+          print(resp);
+          moveToRootTab();
+        } catch (error) {
+          print('카카오톡으로 로그인 실패1 $error');
+
+          // 사용자가 카카오톡 설치 후 디바이스 권한 요청에서 로그인 취소 시
+          // 의도적 실패로 간주 카카오계정으로 로그인 시도 없이 로그인 취소 처리
+          if (error is PlatformException && error.code == 'CANCLED') {
+            return;
+          }
+
+          // 카카오에 연결된 카카오 계정이 없을 때, 카카오 계정 로그인 호출
+          try {
+            resp = await UserApi.instance.loginWithKakaoAccount();
+            print(resp);
+            moveToRootTab();
+          } catch (error) {
+            print('카카오 계정으로 로그인 실패2 $error');
+            return;
+          }
+        }
+      } else {
+        try {
+          resp = await UserApi.instance.loginWithKakaoAccount();
+          print(resp);
+          moveToRootTab();
+        } catch (error) {
+          print('카카오 계정으로 로그인 실패3 $error');
+          return;
+        }
+      }
     }
 
     return DefaultLayout(
