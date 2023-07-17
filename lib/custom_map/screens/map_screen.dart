ef6ea18411b7febabb1efa6759dd5af7ed547ee1
hexaspace <hexa6do@naver.com>
2023-07-17 20:05:48 +0900

    [TAS-89] Feat: custom marker with image and text
    - marker의 icon 속성에 BitmapDescriptor 객채를 만들어서 커스텀 한다.
    - canvas를 활용하여 이미지, text style를 넣는다.
    - canvas -> image-> bytes 변환을 통해 BitmapDescriptor를 만든다

--- lib/custom_map/screens/map_screen.dart
+++ lib/custom_map/screens/map_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:frontend/custom_map/components/marker_icon.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
@@ -14,21 +15,10 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   late GoogleMapController googleMapController;
   final List<Marker> markers = [];
+  int dummy = 2;
   @override
   void initState() {
     super.initState();
-    markers.add(
-      Marker(
-        markerId: MarkerId("1"),
-        draggable: true,
-        onTap: () => print("Marker!"),
-        position: LatLng(37.540853, 127.078971),
-        infoWindow: InfoWindow(
-          title: 'Marker Title Fourth ',
-          snippet: 'My Custom Subtitle',
-        ),
-      ),
-    );
   }
 
   CameraPosition initCameraPosition = const CameraPosition(
@@ -49,14 +39,55 @@ class _MapScreenState extends State<MapScreen> {
           child: GoogleMap(
             mapType: MapType.normal, // hybrid, normal
             initialCameraPosition: initCameraPosition,
+            onMapCreated: (controller) {
+              setState(() {
+                googleMapController = controller;
+              });
+            },
             mapToolbarEnabled: true,
             buildingsEnabled: false,
             myLocationButtonEnabled: true,
             myLocationEnabled: true, // 내 위치를 중앙 파란점 + 방향 화살표
             compassEnabled: false, // 맵 회전시 다시 북쪽을 향하게하는 나침반
             markers: Set.from(markers),
+            onTap: (LatLng latLng) {
+              googleMapController.animateCamera(
+                CameraUpdate.newLatLng(
+                  initCameraPosition.target,
+                ),
+              );
+            },
           ),
         ),
+        ElevatedButton(
+          onPressed: () async {
+            print("추가!@!!!");
+            Random random = Random();
+            int randomNumber = random.nextInt(100); // 0부터 99까지의 랜덤 숫자 생성
+            Marker marker = Marker(
+              markerId: MarkerId(randomNumber.toString()),
+              draggable: true,
+              onTap: () => print("Marker!"),
+              position: LatLng(37.540853, 127.078971),
+              icon: await createMarkerIcon(
+                'assets/images/profile_pictures/user_profile.jpeg',
+                '휘서',
+                Size(200.0, 200.0),
+              ),
+            );
+            setState(() {
+              markers.add(marker);
+            });
+          },
+          child: Text("maker"),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                markers.clear();
+              });
+            },
+            child: Text("marker clear")),
       ],
     );
   }
