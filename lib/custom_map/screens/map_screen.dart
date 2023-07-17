c6c7cb9d119e88d6277221debba95072b9a1bc80
hexaspace <hexa6do@naver.com>
2023-07-17 17:01:51 +0900

    [TAS-89] Feat: add a const maker in map

--- lib/custom_map/screens/map_screen.dart
+++ lib/custom_map/screens/map_screen.dart
@@ -12,61 +12,51 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  late GoogleMapController googleMapController;
+  final List<Marker> markers = [];
+  @override
+  void initState() {
+    super.initState();
+    markers.add(
+      Marker(
+        markerId: MarkerId("1"),
+        draggable: true,
+        onTap: () => print("Marker!"),
+        position: LatLng(37.540853, 127.078971),
+        infoWindow: InfoWindow(
+          title: 'Marker Title Fourth ',
+          snippet: 'My Custom Subtitle',
+        ),
+      ),
+    );
+  }
+
+  CameraPosition initCameraPosition = const CameraPosition(
+    // 건국대 position
+    target: LatLng(
+      37.540853,
+      127.078971,
+    ),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
-    final Completer<GoogleMapController> googleMapController =
-        Completer<GoogleMapController>();
-    Set<Marker> mapMarkers = {};
-
-    const CameraPosition initCameraPosition = CameraPosition(
-      // 건국대 position
-      target: LatLng(
-        37.540853,
-        127.078971,
-      ),
-      zoom: 14.4746,
-    );
-    // void addMarkerRandomFromInitPosition() {
-    //   int randomInt = Random().nextInt(100);
-    //   LatLng latLng = LatLng(
-    //     37.540853 + (randomInt * 0.001),
-    //     127.078971 + (randomInt * 0.001),
-    //   );
-    //   Marker marker = Marker(
-    //     markerId: MarkerId(randomInt.toString()),
-    //     position: latLng,
-    //   );
-    //   print('Location ${randomInt}: ${latLng.latitude}, ${latLng.longitude}');
-
-    //   setState(() {
-    //     mapMarkers.add(marker);
-    //   });
-    // }
-
     return Column(
       children: [
         SizedBox(
           height: 500,
-          child: Stack(children: [
-            GoogleMap(
-              mapType: MapType.normal, // hybrid, normal
-              initialCameraPosition: initCameraPosition,
-              onMapCreated: (GoogleMapController controller) {
-                googleMapController.complete(controller);
-              },
-              myLocationEnabled: true, // 내 위치를 중앙 파란점 + 방향 화살표
-              myLocationButtonEnabled: true, // 우측 상단 내위치로 버튼
-              compassEnabled: true, // 맵 회전시 다시 북쪽을 향하게하는 나침반
-              mapToolbarEnabled: false, // 모르겠음
-              markers: mapMarkers,
-            ),
-          ]),
+          child: GoogleMap(
+            mapType: MapType.normal, // hybrid, normal
+            initialCameraPosition: initCameraPosition,
+            mapToolbarEnabled: true,
+            buildingsEnabled: false,
+            myLocationButtonEnabled: true,
+            myLocationEnabled: true, // 내 위치를 중앙 파란점 + 방향 화살표
+            compassEnabled: false, // 맵 회전시 다시 북쪽을 향하게하는 나침반
+            markers: Set.from(markers),
+          ),
         ),
-        // IconButton(
-        //   onPressed: addMarkerRandomFromInitPosition,
-        //   icon: Icon(Icons.add_location_alt_outlined),
-        //   iconSize: 30,
-        // ),
       ],
     );
   }
