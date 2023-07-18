81123cfc33c72ecdad0ecb10ce285ba76b5dbbad
nakevin96 <nakevin96@hanyang.ac.kr>
2023-07-18 22:29:58 +0900

    Feature: flutter web auth 2 적용한 소셜 로그인 코드

--- lib/user/screens/login_screen.dart
+++ lib/user/screens/login_screen.dart
@@ -1,8 +1,10 @@
-import 'dart:io';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
+
 import 'package:frontend/common/components/animated_app_title.dart';
 import 'package:frontend/common/dio/dio.dart';
 import 'package:frontend/common/layouts/default_layout.dart';
@@ -11,6 +13,9 @@ import 'package:frontend/common/secure_storage/secure_storage.dart';
 import 'package:frontend/user/components/kakao_login_button.dart';
 import 'package:frontend/user/screens/register_dialog_screen.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+
+import '../../common/consts/data.dart';
+import '../utils/oauth_apis.dart';
 
 class LoginScreen extends ConsumerStatefulWidget {
   const LoginScreen({super.key});
@@ -62,71 +67,36 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
       );
     }
 
-    void processKakaoLogin() async {
-      //REMOVE: 아직 IOS 플랫폼 등록을 하지 않아 임시로 안드로이드에서만 카카오 로그인을 테스트합니다.
-      if (Platform.isIOS) {
-        moveToRootTab();
-        return;
-      }
-      // OAuthToken? resp;
-      // // INFO: 앱 키 해시 확인 코드
-      // // final keyHash = await KakaoSdk.origin;
-      // // print('카카오 키해시: $keyHash');
-      // if (await isKakaoTalkInstalled()) {
-      //   try {
-      //     resp = await UserApi.instance.loginWithKakaoTalk();
-      //   } catch (error) {
-      //     print('카카오톡 앱으로 로그인에 실패하였습니다: Error: $error');
-
-      //     // 사용자가 카카오톡 설치 후 디바이스 권한 요청에서 로그인 취소 시
-      //     // 의도적 실패로 간주 카카오계정으로 로그인 시도 없이 로그인 취소 처리
-      //     if (error is PlatformException && error.code == 'CANCLED') {
-      //       return;
-      //     }
-
-      //     // 카카오에 연결된 카카오 계정이 없을 때, 카카오 계정 로그인 호출
-      //     try {
-      //       resp = await UserApi.instance.loginWithKakaoAccount();
-      //     } catch (error) {
-      //       print('카카오 계정으로 로그인에 실패하였습니다: Error: $error');
-      //       return;
-      //     }
-      //   }
-      // } else {
-      //   try {
-      //     resp = await UserApi.instance.loginWithKakaoAccount();
-      //   } catch (error) {
-      //     print('카카오 계정으로 로그인에 실패하였습니다: Error: $error');
-      //     return;
-      //   }
-      // }
-
-      // await secureStorage.write(
-      //     key: KAKAO_REFRESH_TOKEN_KEY, value: resp.refreshToken);
-      // await secureStorage.write(
-      //     key: KAKAO_ACCESS_TOKEN_KEY, value: resp.accessToken);
-      // await secureStorage.write(
-      //     key: KAKAO_REFRESH_TOKEN_EXPIRES_AT_KEY,
-      //     value: resp.refreshTokenExpiresAt.toString());
-
+    Future<void> signInOAuth(String api) async {
       try {
-        final user = await UserApi.instance.me();
-        //final isRegistered = await requestCheckUserRegistered(user);
-        if (false) {
-          //TODO: 사용자가 이미 가입되어 있는 상태라면 바로 Zeppy에
-          // Refresh Token, AccessToken받는 request 호출하기
-          moveToRootTab();
-        } else {
-          //TODO: 사용자가 가입되어 있지 않은 상태라면 사용자 실명 받는 팝업 띄우기
-          showRegisterDialog();
-        }
-      } catch (error) {
-        throw Exception('사용자 정보 요청 실패 $error');
+        final uri = Uri.parse('$api?redirect_url=$APP_SCHEME');
+        log(uri.toString());
+        final webAuthResp = await FlutterWebAuth2.authenticate(
+          url: uri.toString(),
+          callbackUrlScheme: APP_SCHEME,
+        );
+        log('hi');
+        log(webAuthResp);
+
+        final accessToken =
+            Uri.parse(webAuthResp).queryParameters[ACCESS_TOKEN_KEY];
+        final refreshToken =
+            Uri.parse(webAuthResp).queryParameters[REFRESH_TOKEN_KEY];
+
+        await secureStorage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+        await secureStorage.write(key: REFRESH_TOKEN_KEY, value: refreshToken);
+        moveToRootTab();
+      } catch (e) {
+        log(e.toString());
       }
     }
 
+    void oAuthLoginPressed(API api) {
+      signInOAuth(getApiUri(api));
+    }
+
     void onKakaoLoginButtonClick() {
-      processKakaoLogin();
+      oAuthLoginPressed(API.googleLogin);
     }
 
     return DefaultLayout(
