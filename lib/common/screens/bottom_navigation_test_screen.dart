c470c81b88594a0e6a04cf06e957c9246aefaccf
hexaspace <hexa6do@naver.com>
2023-08-03 18:54:09 +0900

    [TAS-123] Feat: register에서 프로필 이미지를 불러오고, 정사각형으로 자르고, 상태관리와 앱내 디랙토리에 저장
    image_picker, image_picker_android 로 갤러리의 이미지 불러오기
    
    path_provider 를 사용해 불러온 이미지를 앱 내 디랙토리에 저장
    
    image_cropper 를 사용해 이미지를 정사각형으로만 crop할 수 있게 함

--- lib/common/screens/bottom_navigation_test_screen.dart
+++ lib/common/screens/bottom_navigation_test_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:frontend/common/consts/data.dart';
 import 'package:frontend/common/riverpod/register_dialog_screen.dart';
+import 'package:image_picker/image_picker.dart';
 
 class BottomNavigationTestScreen extends ConsumerStatefulWidget {
   final String testScreenName;
@@ -15,10 +19,15 @@ class BottomNavigationTestScreen extends ConsumerStatefulWidget {
 
 class _BottomNavigationTestScreenState extends ConsumerState<BottomNavigationTestScreen> {
   String? userName;
+// image picker 를 할 객체
+  final ImagePicker picker = ImagePicker();
+  // 선택된 단일 이미지를 담을 곳
+  File? image;
 
   @override
   Widget build(BuildContext context) {
     final userNameWatch = ref.watch(registeredUserInfoProvider).userName;
+    final userProfileImagePathWatch = ref.watch(registeredUserInfoProvider).userProfileImagePath;
     return SizedBox(
       width: MediaQuery.of(context).size.width,
       child: Column(
@@ -26,6 +35,19 @@ class _BottomNavigationTestScreenState extends ConsumerState<BottomNavigationTes
         children: [
           if (userNameWatch != null) Text('$userNameWatch님 환영합니다!'),
           Text('테스트 전환 화면: ${widget.testScreenName}'),
+          userProfileImagePathWatch == MY_PROFILE_DEFAULT_IMAGE_PATH
+              ? Image.asset(
+                  userProfileImagePathWatch,
+                  height: 200,
+                  width: 200,
+                  fit: BoxFit.cover,
+                )
+              : Image.file(
+                  File(userProfileImagePathWatch),
+                  height: 200,
+                  width: 200,
+                  fit: BoxFit.cover,
+                )
         ],
       ),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import file_selector_macos
 import flutter_secure_storage_macos
 import flutter_web_auth_2
 import geolocator_apple
+import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 import window_to_front
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
