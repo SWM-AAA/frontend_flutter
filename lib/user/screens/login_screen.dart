9219075ba2634ae378ba266dcb1f2882bae1b633
hexaspace <hexa6do@naver.com>
2023-08-04 17:37:14 +0900

    [TAS-116] Design: google 로그인 디자인 추가 및 카카오로그인도 같은 크기로 변경

--- lib/user/screens/login_screen.dart
+++ lib/user/screens/login_screen.dart
@@ -10,6 +10,7 @@ import 'package:frontend/common/dio/dio.dart';
 import 'package:frontend/common/layouts/default_layout.dart';
 import 'package:frontend/common/screens/root_tab.dart';
 import 'package:frontend/common/secure_storage/secure_storage.dart';
+import 'package:frontend/user/components/google_login_button.dart';
 import 'package:frontend/user/components/kakao_login_button.dart';
 import 'package:frontend/user/screens/register_dialog_screen.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
@@ -118,10 +119,13 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                   children: [
                     const AnimatedAppName(),
                     const SizedBox(
-                      height: 12.0,
+                      height: 82.0,
                     ),
                     KakaoLoginButton(
-                      onPressed: showRegisterDialog,
+                      onPressed: onKakaoLoginButtonClick,
+                    ),
+                    GoogleLoginButton(
+                      onPressed: onKakaoLoginButtonClick,
                     ),
                   ],
                 ),
