00297d24c1fafcffdadd0aae37431aadbd8d9388
hexaspace <hexa6do@naver.com>
2023-09-07 14:17:07 +0900

    [TAS-189] Feat: 친구 리스트 모델 생성

---
+++ lib/friend/model/friend_model.dart
@@ -0,0 +1,41 @@
+class FriendListModel {
+  late List<FriendModel> friendRequestList;
+  FriendListModel({
+    required this.friendRequestList,
+  });
+  factory FriendListModel.fromJson(List<dynamic> json) {
+    final List<FriendModel> friendRequestList = [];
+    json.forEach(
+      (element) {
+        final friendRequest = FriendModel.fromJson(
+          element,
+        );
+        friendRequestList.add(friendRequest);
+      },
+    );
+    return FriendListModel(
+      friendRequestList: friendRequestList,
+    );
+  }
+}
+
+class FriendModel {
+  late String userId;
+  late String nickname;
+  late String userTag;
+  late String imageUrl;
+  FriendModel({
+    required this.userId,
+    required this.nickname,
+    required this.userTag,
+    required this.imageUrl,
+  });
+  factory FriendModel.fromJson(Map<String, dynamic> json) {
+    return FriendModel(
+      userId: 'user_' + json['userId'].toString(),
+      nickname: json['nickname'],
+      userTag: json['userTag'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+}
