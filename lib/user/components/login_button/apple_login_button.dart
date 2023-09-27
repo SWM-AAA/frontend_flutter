993bdbcafb62601274c2dbd28005a9d7d64e7415
kkanghh <attsky2@gmail.com>
2023-09-27 16:39:33 +0900

    [TAS-208] 애플 로그인 버튼 디자인 / 다른 로그인 버튼도 같은 size로 변경

--- lib/user/components/login_button/apple_login_button.dart
+++ lib/user/components/login_button/apple_login_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class AppleLoginButton extends StatelessWidget {
   final double? width;
@@ -6,19 +7,41 @@ class AppleLoginButton extends StatelessWidget {
 
   const AppleLoginButton({
     Key? key,
-    this.width = 320,
+    this.width = 280,
     this.onPressed,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.black,
+      ),
       width: width,
-      height: 60,
-      child: IconButton(
-        onPressed: onPressed,
-        icon: Image.asset(
-          'assets/images/apple/login_button.png',
+      height: 48,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 32),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/svg/Apple_logo_white.svg',
+              width: 20,
+              height: 20,
+            ),
+            const SizedBox(
+              width: 8,
+            ),
+            const Text(
+              'Apple로 로그인',
+              style: TextStyle(
+                  fontFamily: 'Pretendard',
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w700),
+            )
+          ],
         ),
       ),
     );
--- lib/user/components/login_button/kakao_login_button.dart
+++ lib/user/components/login_button/kakao_login_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class KakaoLoginButton extends StatelessWidget {
   final double? width;
@@ -12,12 +13,41 @@ class KakaoLoginButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: width,
-      child: InkWell(
-        onTap: onPressed,
-        child: Image.asset(
-          'assets/images/kakao/kakao_login_button.png',
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: const Color(0xfffee500),
+        ),
+        width: width,
+        height: 48,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8.0,
+            horizontal: 32,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset(
+                'assets/svg/kakao.svg',
+                width: 20,
+                height: 20,
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              const Text(
+                '카카오 로그인',
+                style: TextStyle(
+                    fontFamily: 'Pretendard',
+                    color: Color(0xff000000),
+                    fontSize: 15,
+                    fontWeight: FontWeight.w700),
+              )
+            ],
+          ),
         ),
       ),
     );
